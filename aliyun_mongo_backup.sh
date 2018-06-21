4b1b209088083a65a2d610c6fd15c71dedf77a2f
applegit11 <13774437301@163.com>
2018-06-21 10:59:52 +0800

    add aliyun_mongodb_backup

---
+++ aliyun_mongo_backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#author:apple
+#用时45min
+
+
+DUMP=/alidata/mongodb/bin/mongodump
+BACK_DIR=/mnt/mongodbbak
+DATE=`date +%F`
+DB_USER=root
+DB_PASS=1111
+DAYS=3
+log_file=$BACK_DIR/backup.log
+
+echo `date +%F%H:%M:%S` > $log_file
+$DUMP --host s-wz55555ac91f8555.mongodb.rds.aliyuncs.com:3777 --authenticationDatabase admin -u $DB_USER -p $DB_PASS -d scbd --gzip -o $BACK_DIR/$DATE &>> $log_file
+find $DUMP/DATE -mtime +$DAYS -delete
