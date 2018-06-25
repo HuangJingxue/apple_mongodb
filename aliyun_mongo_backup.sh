68becc0eb545b7776ae41fb336955dc62f57ea61
applegit11 <13774437301@163.com>
2018-06-25 10:52:52 +0800

    update backup file

--- aliyun_mongo_backup.sh
+++ aliyun_mongo_backup.sh
@@ -13,4 +13,4 @@ log_file=$BACK_DIR/backup.log
 
 echo `date +%F%H:%M:%S` > $log_file
 $DUMP --host s-wz55555ac91f8555.mongodb.rds.aliyuncs.com:3777 --authenticationDatabase admin -u $DB_USER -p $DB_PASS -d scbd --gzip -o $BACK_DIR/$DATE &>> $log_file
-find $DUMP/DATE -mtime +$DAYS -delete
+find $DUMP -type d -mtime +$DAYS3 -execdir rm -rf {} \;
