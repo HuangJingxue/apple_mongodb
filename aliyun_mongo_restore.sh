39e5b5cf12227c4ca2b3f80ff83cbfd9016efaa0
applegit11 <13774437301@163.com>
2018-06-25 10:42:47 +0800

    update mongodb restore

--- aliyun_mongo_restore.sh
+++ aliyun_mongo_restore.sh
@@ -1,3 +1,5 @@
+[root@iZwz97n253zzwjsmyn9kxaZ mongodbbak]# vim mongo_restore.sh 
+
 #!/bin/bash
 #author:apple
 #对mongos连接CPU性能 network性能 connection性能有影响
@@ -6,8 +8,26 @@ RESTORE=/alidata/mongodb/bin/mongorestore
 BACK_DIR=/mnt/mongodbbak
 DATE=`date +%F`
 DB_USER=root
-DB_PASS=111
+DB_PASS=aaaaaaaaaaa
 log_file=$BACK_DIR/restore.log
 
+
 echo `date +%F%H:%M:%S` > $log_file
-$RESTORE -h s-wz98888adae78888.mongodb.rds.aliyuncs.com:3777 --authenticationDatabase=admin -u $DB_USER -p $DB_PASS -d scbd --drop --gzip --dir=$BACK_DIR/$DATE/scbd/ &>> $log_file
+$RESTORE -h s-wz971034.mongodb.rds.aliyuncs.com:3717 --authenticationDatabase=admin -u $DB_USER -p $DB_PASS -d scbd --drop --gzip --dir=$BACK_DIR/$DATE/scbd/ &>> $log_file
+
+cat > afile << ENDF
+db.runCommand( { enablesharding : "scbd" } )
+db.runCommand( { shardcollection : "scbd.AccessToken",key : { "userId" : 1, "_id" : 1} } )
+db.runCommand( { shardcollection : "scbd.attendanceRegister",key : { "employeeId" : 1, "day" : 1} } )
+db.runCommand( { shardcollection : "scbd.driverOrder",key : { "_id" : 1} } )
+db.runCommand( { shardcollection : "scbd.employeeVisitlog",key : { "_id" : 1} } )
+db.runCommand( { shardcollection : "scbd.flow",key : { "typeId" : 1, "_id" : 1} } )
+db.runCommand( { shardcollection : "scbd.goodsCirculationLog",key : { "employeeId" : 1, "_id" : 1} } )
+ENDF
+
+
+while read line
+do
+    echo $line | mongo s-wz971034.mongodb.rds.aliyuncs.com:3717/admin  --authenticationDatabase admin -u $DB_USER  -p $DB_PASS
+    #echo $line
+done < afile
